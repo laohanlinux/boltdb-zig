11e5b4fb15d3e66b77a2d4372a47cead1f0db93a
zhenghuarong <zhenghuarong@joyy.com>
2024-05-14 19:43:51 +0800

    :cat:

---
+++ src/assert.zig
@@ -0,0 +1,12 @@
+const std = @import("std");
+
+pub fn assert(expect: bool, comptime fmt: []const u8, args: anytype) void {
+    if (!expect) {
+        std.debug.print(fmt, args);
+        std.debug.assert(expect);
+    }
+}
+
+test "assert" {
+    assert(std.time.milliTimestamp() < 0, "it should be happen, {d}\n", .{std.time.milliTimestamp()});
+}
