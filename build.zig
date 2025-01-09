b57e4db0144d88d11d95853775279692e454b743
Rg <zhenghuarong@joyy.com>
2025-01-09 18:40:17 +0800

    :cat:

--- build.zig
+++ build.zig
@@ -15,6 +15,10 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    const boltdb_module = b.addModule("boltdb", .{
+        .source_file = .{ .path = "src/root.zig" },
+    });
+
     const lib = b.addStaticLibrary(.{
         .name = "boltdb-zig",
         // In this case the main source file is merely a path, however, in more
@@ -36,6 +40,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    exe.addModule("boltdb", boltdb_module);
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
